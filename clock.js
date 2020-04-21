9d0f19720b6fe58cc24b48fdc71b9902d9fc9be0
강연욱 <rokkyw@khu.ac.kr>
2020-04-21 18:49:50 +0900

    날짜코드추가

--- clock.js
+++ clock.js
@@ -1,5 +1,16 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1"),
+    dateTitle = document.querySelector(".js-date");
+
+function getDate() {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    dateTitle.innerText = `${year} / ${month < 10 ? `0${month}`:month} / ${day < 10 ? `0${day}`:day}`;
+    
+
+}
 
 function getTime() {
     const date = new Date();
@@ -11,6 +22,7 @@ function getTime() {
 
 function init() {
     getTime();
+    getDate();
     setInterval(getTime, 1000); //1000은 1초이다. 밀리세컨드 단위.
 }
 init();
